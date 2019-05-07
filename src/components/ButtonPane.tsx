6b41d41acfa9c9ecb7dcd498823178f48badfb6e
devps <devps@remote.dbh.support>
2019-05-07 14:22:57 +0200

    ButtonPane in Typescript

--- src/components/ButtonPane.tsx
+++ src/components/ButtonPane.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Layout, Dropdown, Input, Button } from 'element-react/next';
 
-function BtnList(props) {
+interface btnProps{
+    clicks : Function;
+
+}
+
+function BtnList(props: btnProps) {
     const btns = ["Copy", "Delete", "New"];
     const items = btns.map((curr) => 
-        <Button onClick={(e) => props.clicks(curr.toString(), e)} type="secondary" style={{ paddingTop: '7px', paddingBottom: '7px'}} key={curr.toString()}>
+        <Button onClick={(e: Event) => props.clicks(curr.toString(), e)} type="secondary" style={{ paddingTop: '7px', paddingBottom: '7px'}} key={curr.toString()}>
             {curr}
         </Button>
     );
@@ -13,16 +18,27 @@ function BtnList(props) {
     )
 }
 
-class ButtonPane extends React.Component {
-    constructor(props) {
+interface IProps {
+    searchAble: boolean;
+    buttonClicks: Function;
+    search: Function;
+}
+
+interface IState {
+    searchInput: string;
+    searchAble: boolean;
+}
+
+class ButtonPane extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
         super(props);
         this.state = {
-            searchText: '',
+            searchInput: '',
             searchAble: props.searchAble,
         };
     }
 
-    handleChange(value) {
+    handleChange(value: string) {
         this.setState({
             searchInput: value
         },this.props.search(value));
@@ -34,7 +50,7 @@ class ButtonPane extends React.Component {
                 {true &&  // hasCommands etc.
                     <Layout.Row style={{background:'#323639'}}>
                         <Layout.Col style={{marginTop: '1em'}} span={this.state.searchAble?4:0}>
-                            <Input style={{paddingLeft: '1em'}} value={this.state.searchInput} placeholder="search" size="small" onChange={(e) => this.handleChange(e)}/>
+                            <Input style={{paddingLeft: '1em'}} value={this.state.searchInput} placeholder="search" size="small" onChange={(e: string) => this.handleChange(e)}/>
                         </Layout.Col>
                         <Layout.Col span={this.state.searchAble?20:24} style={{textAlign:'end'}}>
                             {false &&
@@ -52,7 +68,7 @@ class ButtonPane extends React.Component {
                                     </Button>
                                 </Dropdown>
                             }
-                            <BtnList clicks={(e) => this.props.buttonClicks(e)} />
+                            <BtnList clicks={(e: any) => this.props.buttonClicks(e)} />
                         </Layout.Col>
                     </Layout.Row>
                 }
